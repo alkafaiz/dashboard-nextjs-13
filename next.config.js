09bee7ab029b4d02f69e730574f4b1d3cca56556
Faiz Alkautsar <alkafaiz99@gmail.com>
2023-07-28 23:51:29 +0800

    configure redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/signin',
+                permanent: true,
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
